434aa3ffd39ed80bf72bfa2d416e4a625bba8064
utsavbhattarai007 <ninja08469@gmail.com>
2023-12-09 22:31:20 +0545

    Navbar coded

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { HiPaperAirplane } from "react-icons/hi";
+import style from "../css/components/Navbar.module.css"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <>
+      <div className={style.navbar_container}>
+        <div className={style.navbar_wrapper}>
+          <div className={style.navbar_logo}>
+            <svg width="0" height="0">
+              <linearGradient id="gradient" x1="100%" y1="100%" x2="0%" y2="0%">
+                <stop stopColor="#f96f10" offset="0%" />
+                <stop stopColor="#ef5088" offset="100%" />
+              </linearGradient>
+            </svg>
+            <HiPaperAirplane
+              className={style.navbar_icon}
+              style={{ fill: "url(#gradient)" }}
+            />
+            <h1 className={style.navbar_title}>Apexa</h1>
+          </div>
+          <div className={style.btn_wrapper}>
+            <div className={style.btn1}>Sign in</div>
+            <div className={style.btn2}>Sign up</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Navbar
