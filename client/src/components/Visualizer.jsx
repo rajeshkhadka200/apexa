2d6cfb0cb0793d36741822d3e408f7ef6245839b
rajeshkhadka <rajeshkhadkaofficia@gmail.coml>
2023-12-14 14:35:43 +0545

    managing merge conflict

--- client/src/components/Visualizer.jsx
+++ client/src/components/Visualizer.jsx
@@ -257,9 +257,6 @@ const Visualizer = ({ search, onSearch, handleSearch, data }) => {
         </div>
       </div>
 
-      {
-        //model
-      }
       <Modal
         disableAutoFocus={true}
         open={open}
@@ -269,7 +266,7 @@ const Visualizer = ({ search, onSearch, handleSearch, data }) => {
       >
         <div className={styles.summary_con}>
           <div className={styles.summary_title}>
-            Summary{" "}
+            Summary
             <span className={styles.sub_text}>
               {loading && "(Magic on the way... Just Wait)"}
             </span>
