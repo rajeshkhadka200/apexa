6c918d3007b1697158fdd635240fcf0df8a2f93a
utsavbhattarai007 <ninja08469@gmail.com>
2023-12-17 21:05:30 +0545

    coded the server connection to local instance of mindsdb

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 import express from "express";
 import { connectMindsDB } from "./utils/mindsdb.js";
+import MindsDB from "mindsdb-js-sdk";
+
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3000;
 
 //connection to mindsdb local instance
 connectMindsDB();
+
+// const getDb = async () => {
+//   const currentDb = await MindsDB.default.Databases.getDatabase(
+//     "mindsdb_youtube"
+//   );
+//   console.log(currentDb)
+// }
+
+// getDb();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
--- server/utils/mindsdb.js
+++ server/utils/mindsdb.js
@@ -2,10 +2,10 @@ import MindsDB from "mindsdb-js-sdk";
 
 export const connectMindsDB = async () => {
   try {
-    await MindsDB.connect({
+    await MindsDB.default.connect({
       host: "http://127.0.0.1:47334",
     });
-    console.log("connected");
+    console.log("MindsDB connected");
   } catch (error) {
     // Failed to connect to local instance
     console.log(error);
