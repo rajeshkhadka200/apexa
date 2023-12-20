a41fee24a938a1a62332db92bd1b244eaeb33915
rajeshkhadka <rajeshkhadkaofficia@gmail.coml>
2023-12-20 17:36:35 +0545

    added auth

--- client/src/layouts/Nav.jsx
+++ client/src/layouts/Nav.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
+import { Navigate } from "react-router-dom";
 
 const Nav = () => {
+  const accessToken = localStorage.getItem("access");
+
+  useEffect(() => {
+    if (accessToken) {
+      // <Navigate to="/" />;
+    }
+  }, []);
+
   return (
     <>
       <Navbar />
