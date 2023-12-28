90dac8e72fc605b4df8edfb34844324adda3a151
rajeshkhadka <rajeshkhadkaofficia@gmail.coml>
2023-12-28 22:26:37 +0545

    small fix

--- server/index.js
+++ server/index.js
@@ -27,11 +27,6 @@ app.use("/api/user", UserRouter);
 app.use("/api/yt", YtRouter);
 app.use("/api/hashnode", HashnodeRouter);
 
-//test route
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server is listening on port ${PORT}`);
